0b74e07b00c1188fe0d515ca15d91127d89caaad
Anton Babkin <6345499+antonbabkin@users.noreply.github.com>
2024-04-26 11:04:44 -0500

    Add test total_supply = total_use

--- tests/testthat/test-bea_io.R
+++ tests/testthat/test-bea_io.R
@@ -15,3 +15,21 @@ test_that("get() calls produce no errors for all data keys", {
     expect_no_error(bea_io_get(key))
   }
 })
+
+
+for (key in bea_io_ls("curr_sup*")) {
+  test_that(glue::glue("total supply equals total use in {key}"), {
+    # supply table rows are all commodities + total
+    total_supply <- bea_io_get(key) |>
+      dplyr::filter(col_name == "Total product supply (purchaser prices)") |>
+      dplyr::pull(value) |>
+      tidyr::replace_na(0)
+    # use table rows are all commodities, total and extras
+    total_use <- bea_io_get(sub("_sup_", "_use_", key)) |>
+      dplyr::filter(col_name == "Total use of products") |>
+      dplyr::pull(value) %>%
+      `[`(1:length(total_supply)) |> # only keep commodities and total, ignore extras
+      tidyr::replace_na(0)
+    expect_equal(total_supply, total_use, tolerance = 1e-7)
+  })
+}
