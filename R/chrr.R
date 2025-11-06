0b2085f63f33de9f326fac5dc8fddd52d41b2ddf
IsabellaJiiang <Isabella.jyn@gmail.com>
2025-11-06 01:41:28 -0600

    Skip human-readable header (already save as desc)

--- R/chrr.R
+++ R/chrr.R
@@ -16,7 +16,14 @@ chrr_get <- function(key) {
   }
 
   raw <- get("chrr", this_meta$depends)
+
+  # CHRR data has a human-readable header in line 1 followed by variable name in line 2
+  # line 1 are already captured in YAML schema `desc`, so we skip line 1
+  header <- readr::read_csv(raw, skip = 1, n_max = 1, col_names = FALSE, show_col_types = FALSE)
+  col_names <- as.character(header[1, ])
+  y <- readr::read_csv(raw, skip = 2, col_names = col_names, show_col_types = FALSE)
+
+  colnames(df) <- trimws(colnames(df))
   types <- purrr::map(this_meta$schema, \(x) x$type)
-  y <- readr::read_csv(raw, col_types = types)
   y
 }
