0999696f0358fabd3cd20568d42b66ae525ad734
Anton Babkin <6345499+antonbabkin@users.noreply.github.com>
2025-12-16 11:09:56 -0600

    Add RUCA codes to ers_rural collection

--- R/ers_rural.R
+++ R/ers_rural.R
@@ -14,26 +14,53 @@ ers_rural_get <- function(key) {
 
   raw <- get("ers_rural", this_meta$depends)
   types <- purrr::map(this_meta$schema, \(x) x$type)
-  # read_excel() uses different type names and does not distinguish integer and double
-  excel_types <- purrr::map_chr(types, \(x) switch(
-    x,
-    logical = "logical",
-    character = "text",
-    integer = "numeric",
-    double = "numeric"
-  ))
 
   # read RUC and UI tables
   if (key %in% c("ruc_1974", "ruc_1983", "ruc_1993", "ruc_2003", "ruc_2003pr", "ruc_2013", "ruc_2023",
-                 "ui_2003pr", "ui_2003_1993", "ui_2013", "ui_2024")) {
+                 "ui_2003pr", "ui_2003_1993", "ui_2013", "ui_2024",
+                 "ruca_1990", "ruca_2000", "ruca_2010", "ruca_2010zip", "ruca_2020", "ruca_2020zip")) {
     # if sheet not specified in meta, it will be "NULL" and default to first sheet
     sheet <- this_meta$read$sheet
-    df <- readxl::read_excel(raw, sheet = sheet, col_names = names(types), skip = 1, col_types = excel_types)
+    # if skip rows not specified in meta, skip 1
+    skip <- this_meta$read$skip
+    if (is.null(skip)) skip <- 1
+    # read everything as text and convert manually
+    # for numeric types, read_excel does not distinguish integer and double
+    # reading text columns as numeric prints warning for each value
+    df <- readxl::read_excel(raw, sheet = sheet, col_names = names(types), skip = skip, col_types = "text")
     for (col in names(df)) {
       if (types[[col]] == "integer") {
         df[[col]] <- as.integer(df[[col]])
+      } else if (types[[col]] == "double") {
+        df[[col]] <- as.double(df[[col]])
+      } else if (types[[col]] == "logical") {
+        df[[col]] <- as.logical(df[[col]])
       }
     }
+
+    if (key == "ruca_2000") {
+      # secondary code is stored as number in excel, and reads with too many digits, e.g. 9.1999999999999993 instead of 9.2
+      df[["ruca2_code"]] <- df[["ruca2_code"]] |>
+        as.double() |>
+        round(digits = 1) |>
+        formatC(digits = 1, format = "f") |>
+        stringr::str_remove("\\.0")
+      # state, county and tract components of the FIPS code are missing leading zeroes
+      df[["st_code"]] <- df[["st_code"]] |>
+        stringr::str_pad(width = 2, pad = "0")
+      df[["cty_code"]] <- df[["cty_code"]] |>
+        stringr::str_pad(width = 3, pad = "0")
+      df[["tract_code"]] <- df[["tract_code"]] |>
+        stringr::str_pad(width = 6, pad = "0")
+    } else if (key == "ruca_2010") {
+      # secondary code is stored as number in excel, and reads with too many digits, e.g. 9.1999999999999993 instead of 9.2
+      df[["ruca2_code"]] <- df[["ruca2_code"]] |>
+        as.double() |>
+        round(digits = 1) |>
+        formatC(digits = 1, format = "f") |>
+        stringr::str_remove("\\.0")
+    }
+
   }
 
   df
