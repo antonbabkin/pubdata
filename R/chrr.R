efa50884bc6127769654a1e6bcda617c3debf5d4
Anton Babkin <6345499+antonbabkin@users.noreply.github.com>
2026-02-19 12:25:47 -0600

    Add all remaining CHRR years, pivot longer, doc page

--- R/chrr.R
+++ R/chrr.R
@@ -7,20 +7,35 @@
 #'
 #' @return Tidy table or path to raw file.
 chrr_get <- function(key) {
-  this_meta <- meta("chrr", key, print = FALSE)
+  key_meta <- meta("chrr", key, print = FALSE)
+  stopifnot(key_meta$type == "table")
 
-  if (this_meta$type == "raw") {
-    path <- pubdata_path("chrr", this_meta$path)
-    download_file(this_meta$url, path)
-    return(path)
+  raw <- get("chrr", key_meta$depends)
+
+  # 2012 has duplicate line 1 descriptions, read 2 lines without column names
+  var_desc <- readr::read_csv(raw, col_names = FALSE, col_types = list(.default = "c"), n_max = 2) |>
+    tibble::add_column(col = c("description", "variable"), .before = 1) |>
+    tidyr::pivot_longer(!col) |>
+    tidyr::pivot_wider(id_cols = "name", names_from = "col") |>
+    dplyr::select(variable, description)
+
+  types <- list(
+    statecode = "c",
+    countycode = "c",
+    fipscode = "c",
+    state = "c",
+    county = "c",
+    year = "i",
+    .default = "d")
+  if (key_meta$keys$year %in% c("2024", "2025")) {
+    types$county_clustered <- "i"
+  } else {
+    types$county_ranked <- "i"
   }
 
-  raw <- get("chrr", this_meta$depends)
-
-  # CHRR data has a human-readable header in line 1 followed by variable name in line 2
-  # line 1 are already captured in YAML schema `desc`, so we skip line 1
-  y <- readr::read_csv(raw, skip = 1, show_col_types = FALSE)
-  colnames(y) <- trimws(colnames(y))
-  types <- purrr::map(this_meta$schema, \(x) x$type)
+  y <- readr::read_csv(raw, skip = 1, col_types = types) |>
+    tidyr::pivot_longer(starts_with("v"), names_to = "variable") |>
+    dplyr::left_join(var_desc, by = "variable") |>
+    dplyr::relocate(value, .after = description)
   y
 }
