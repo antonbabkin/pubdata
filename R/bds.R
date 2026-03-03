2b4bff4a8097ca7d039b728a608b670c2dd9ed63
Anton Babkin <6345499+antonbabkin@users.noreply.github.com>
2026-03-03 10:54:31 -0600

    Add all 2023 tables to BDS, create suppression columns

--- R/bds.R
+++ R/bds.R
@@ -5,61 +5,29 @@
 #'
 #' @return Tidy table or path to raw file.
 bds_get <- function(key) {
-  this_meta <- meta("bds", key, print = FALSE)
+  key_meta <- meta("bds", key, print = FALSE)
+  stopifnot(key_meta$type == "table")
 
-  if (this_meta$type == "raw") {
-    path <- pubdata_path("bds", this_meta$path)
-    utils::download.file(this_meta$url, mkdir(path))
-    stopifnot(file.exists(path))
-    return(path)
+  raw <- get("bds", key_meta$depends)
+  # read everything as character to preserve suppression flags, then convert types according to the schema
+  df <- readr::read_csv(raw, col_types = list(.default = "c"))
+  suppression_flags <- c("D", "S", "X", "N")
+  for (col in names(df)) {
+    # create a flag column if it exist in the scmema
+    flag_col <- paste0(col, "_f")
+    if (!is.null(key_meta$schema[[flag_col]])) {
+      df <- df %>%
+        dplyr::mutate(!!flag_col := dplyr::if_else(!!sym(col) %in% suppression_flags, !!sym(col), NA), .after = !!sym(col))
+    }
+    # convert numeric cols, dropping non-numeric values already captured in the flag columns
+    if (key_meta$schema[[col]]$type == "integer") {
+      df[[col]] <- readr::parse_integer(df[[col]], na = suppression_flags)
+    } else if (key_meta$schema[[col]]$type == "double") {
+      df[[col]] <- readr::parse_double(df[[col]], na = suppression_flags)
+    }
   }
-
-  raw <- get("bds", this_meta$depends)
-  bds_read(raw, this_meta)
-
-}
-
-
-#' Read BDS table
-bds_read <- function(raw, meta) {
-
-  # TODO: handle missing values
-  # TODO: only specify columns that exist in a given file to avoid "Warning: The following named parsers don't match the column names: ..."
-  df <- readr::read_csv(raw, col_types = readr::cols(
-    year = "i",
-    metro = "c",
-    st = "c",
-    cty = "c",
-    eage = "c",
-    eagecoarse = "c",
-    esizecoarse = "c",
-    sector = "c",
-    firms = "i",
-    estabs = "i",
-    emp = "i",
-    denom ="i",
-    estabs_entry = "i",
-    estabs_entry_rate = "d",
-    estabs_exit = "i",
-    estabs_exit_rate = "d",
-    job_creation = "i",
-    job_creation_births = "i",
-    job_creation_continuers = "i",
-    job_creation_rate_births = "d",
-    job_creation_rate = "d",
-    job_destruction = "i",
-    job_destruction_deaths = "i",
-    job_destruction_continuers = "i",
-    job_destruction_rate_deaths = "d",
-    job_destruction_rate = "d",
-    net_job_creation = "i",
-    net_job_creation_rate = "d",
-    reallocation_rate = "d",
-    firmdeath_firms = "i",
-    firmdeath_estabs = "i",
-    firmdeath_emp = "i"
-  ))
 
   df
 }
+
 
